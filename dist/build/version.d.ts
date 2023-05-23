967333ba23544603b512baf8b96b5fd9bc27cf2d
Tom McLellan <tom@growthclick.com>
2023-05-23 13:34:17 -0700

    Force dist/build

---
+++ dist/build/version.d.ts
@@ -0,0 +1,3 @@
+export declare const SDK_NAME = "sentry.javascript.capacitor";
+export declare const SDK_VERSION = "0.11.4";
+//# sourceMappingURL=version.d.ts.map
---
+++ dist/build/version.js
@@ -0,0 +1,5 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SDK_VERSION = exports.SDK_NAME = void 0;
+exports.SDK_NAME = 'sentry.javascript.capacitor';
+exports.SDK_VERSION = '0.11.4';
+//# sourceMappingURL=version.js.map
